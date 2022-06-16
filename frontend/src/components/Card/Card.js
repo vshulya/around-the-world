6c3dfbf0423393c0426c27371a3e2fc93db124dd
Valya <vshulya@gmail.com>
2022-06-16 10:18:55 -0400

    fix

--- frontend/src/components/Card/Card.js
+++ frontend/src/components/Card/Card.js
@@ -1,12 +1,11 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
 function Card({ card, onCardClick, onCardLike, onCardDelete }) {
 
-  const currentUser = React.useContext(CurrentUserContext);
+  const currentUser = useContext(CurrentUserContext);
   // Определяем, являемся ли мы владельцем текущей карточки
   const isOwn = card.owner === currentUser._id;
-  //const isOwn = card.owner._id === currentUser._id;
 
   // Создаём переменную, которую после зададим в `className` для кнопки удаления
   const cardDeleteButtonClassName = (
@@ -14,7 +13,7 @@ function Card({ card, onCardClick, onCardLike, onCardDelete }) {
   );
 
   // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
-  const isLiked = card.likes.some((i) => i === currentUser._id);
+  const isLiked = card.likes.some(i => i === currentUser._id);
 
   // Создаём переменную, которую после зададим в `className` для кнопки лайка
   const cardLikeButtonClassName = `card__like ${isLiked ? 'card__like_active' : 'card__like_nonactive'}`;
